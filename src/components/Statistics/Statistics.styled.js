bc44dfbdd6229fab0c91902561f1e27e0a6aa9d8
AleksandrGlinskiy <tarieboe1993@gmail.com>
2023-07-17 17:49:55 +0300

    function randomColor add to comment

--- src/components/Statistics/Statistics.styled.js
+++ src/components/Statistics/Statistics.styled.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
-function getRandomColor() {
+/* function getRandomColor() {
   var letters = '0123456789ABCDEF';
   var color = '#';
   for (var i = 0; i < 6; i += 1) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
-}
+} */
 
 export const Container = styled.section`
   background-color: gray;
